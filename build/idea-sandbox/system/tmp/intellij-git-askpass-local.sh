05068a01069dcc78edf863bfe48b0d916a8efeef
sumanth <sumanth@pallamreddy.com>
2023-08-13 20:26:36 -0700

    deleted build dir

--- build/idea-sandbox/system/tmp/intellij-git-askpass-local.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-"/Users/blobfish/Library/Java/JavaVirtualMachines/liberica-11.0.14/bin/java" -cp "/Applications/Android Studio.app/Contents/plugins/git4idea/lib/git4idea-rt.jar:/Applications/Android Studio.app/Contents/lib/xmlrpc-2.0.1.jar:/Applications/Android Studio.app/Contents/lib/commons-codec-1.15.jar" org.jetbrains.git4idea.http.GitAskPassApp "$@"
